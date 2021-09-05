8e9e185c4c1043bc114725c4f4fd8dc268c1f085
TAN KOK KEONG <tankokkeong4488@gmail.com>
2021-09-05 14:55:09 +0800

    search prompt

--- script/script.js
+++ script/script.js
@@ -836,3 +836,50 @@ function unauthorizedAccessed(){
         }
     });
 }
+
+function dashboardSearch() {
+    var search_input = document.getElementById("dashboard-search-input").value;
+    var results_container = document.getElementById("dashboard-search-results-container");
+    var result_item = document.getElementsByClassName("app-search-item");
+    var matching_container = [];
+
+    if (search_input.length > 0) {
+
+        //Display Search Results box
+        results_container.style.display = "";
+    }
+    else
+    {
+        //Remove Search Results box
+        results_container.style.display = "none";
+    }
+
+    for (var i = 0; i < result_item.length; i++) {
+
+        //Convert the results and input to lowercase
+        if (result_item[i].innerHTML.toLowerCase().includes(search_input.toLowerCase())) {
+
+            //Display matching results
+            result_item[i].style.display = "";
+
+            matching_container.push(result_item[i].href);
+            
+        }
+        else
+        {
+            //Remove not matching results
+            result_item[i].style.display = "none";
+        }
+    }
+
+    //Check if the array is empty
+    if (matching_container.length > 0) {
+        //If user press the enter key
+        if (event.keyCode === 13) {
+            event.preventDefault();
+
+            window.location.href = matching_container[0];
+        }
+    }
+    
+}
